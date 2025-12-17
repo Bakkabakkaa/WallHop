58a1ab1b4188b524691218eeedd48cb358e29190
Art'm <kasko.artem45@gmail.com>
2025-12-18 01:45:14 +0300

    game opening scene

--- Assets/Scripts/Game/GlobalConstants.cs
+++ Assets/Scripts/Game/GlobalConstants.cs
@@ -4,4 +4,6 @@ public static class GlobalConstants
     public const string FLOOR_TAG = "Floor";
     public const string PLAYER_TAG = "Player";
     public const string SCORE_PREFS_KEY = "ScorePrefsKey";
+    public const string BEST_SCORE_PREFS_KEY = "BestScorePrefsKey";
+    public const string GAME_SCENE = "game_scene";
 }
---
+++ Assets/Scripts/Game/StartGameScreen.cs
@@ -0,0 +1,25 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameScreen : MonoBehaviour
+{
+    [SerializeField] private ColorProvider _colorProvider;
+    [SerializeField] private TextMeshProUGUI _bestScoreLabel;
+
+    private void Start()
+    {
+        var randomColor = _colorProvider.GetRandomColor();
+        _colorProvider.CurrentColor = randomColor;
+        Camera.main.backgroundColor = randomColor;
+
+        var bestScore = PlayerPrefs.GetInt(GlobalConstants.BEST_SCORE_PREFS_KEY, 0);
+        _bestScoreLabel.text = $"BEST {bestScore.ToString()}";
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync(GlobalConstants.GAME_SCENE);
+    }
+}
